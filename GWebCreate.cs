f37d3739aaa41089f7ac3ae541772348b36b081a
GrandmaSocks <62735552+GrandmaSocks@users.noreply.github.com>
2020-09-07 08:02:45 -0700

    Commented in explanation for parts of the code

--- GWebCreate.cs
+++ GWebCreate.cs
@@ -12,7 +12,7 @@ namespace GWebCreate
             static void Intro()
             {
                 string message = "GWebCreate a C# Application Created by GrandmaSocks";
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Green; // Set console text color to green
 
 
                 for (int i = 0; i < message.Length; ++i)
@@ -21,7 +21,7 @@ namespace GWebCreate
                     Console.Write(message[i]);
                 }
 
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.White; // Reset color to white
 
 
 
@@ -43,24 +43,25 @@ namespace GWebCreate
                         Console.WriteLine("Enter the name the folder should have");
                         string folderInput = Console.ReadLine();
 
-                        string folderPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + "\\" + "" + folderInput;
+                        string folderPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + "\\" + "" + folderInput; // Combines the desktop directory with what the user inputs to create a folder on the desktop with a custom name
                         string imagesFolderPath = folderPath + "\\" + "images";
 
-                        string indexPath = folderPath + "\\" + "index.html";
-                        string stylePath = folderPath + "\\" + "style.css";
-                        string JSPath = folderPath + "\\" + "main.js";
+                        string indexPath = folderPath + "\\" + "index.html"; 
+                        string stylePath = folderPath + "\\" + "style.css"; // The method I chose to create the paths for the files 
+                        string JSPath = folderPath + "\\" + "main.js"; 
 
-                        string[] indexLines = { "<!DOCTYPE html>", "", "<html=\"en\">", "", "<head>", "", "<meta charset=\"UTF-8\">", "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">", "", "<title>Document</title>", "<link rel=\"stylesheet\" href=\"style.css\">", "", "</head>", "", "<body>", "", "</body>", "", "</html>" };
+                        string[] indexLines = { "<!DOCTYPE html>", "", "<html=\"en\">", "", "<head>", "", "<meta charset=\"UTF-8\">", "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">", "", "<title>Document</title>", "<link rel=\"stylesheet\" href=\"style.css\">", "", "</head>", "", "<body>", "", "</body>", "", "</html>" }; // This is the easiest way I could find to write multiple lines into a file
+                        // Every new element in the array is a new line
 
                         string[] styleLines = { ":root{}", "", "h1{}", "", "p{}" };
 
                         string[] jsLines = { };
 
                         System.IO.Directory.CreateDirectory(folderPath);
-                        System.IO.Directory.CreateDirectory(imagesFolderPath);
+                        System.IO.Directory.CreateDirectory(imagesFolderPath); // Create our folders
 
                         File.WriteAllLines(indexPath, indexLines);
-                        File.WriteAllLines(stylePath, styleLines);
+                        File.WriteAllLines(stylePath, styleLines); // Create our files
                         File.WriteAllLines(JSPath, jsLines);
 
                         break;
@@ -72,6 +73,7 @@ namespace GWebCreate
                         Thread.Sleep(500);
                         Console.Clear();
 
+                        // Exact same process here expect some diffrent files
 
                         Console.WriteLine("Enter the name the folder should have");
                         string portfolioFolderInput = Console.ReadLine();
